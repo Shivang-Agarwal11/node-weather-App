ab4be3bae8b95042696ff553c0dae67a3ef4c473
Shivang Agarwal <shivangagarwal11.sa@gmail.com>
2022-02-21 03:43:34 -0500

    set up for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -14,8 +14,8 @@ weatherForm.addEventListener('submit',(event)=>{
             if(data.error)
             messageOne.textContent=data.error
             else{
-                messageTwo.style.fontSize='xx-large'
-                messageTwo.style.fontWeight='900'
+                messageTwo.style.fontSize='x-large'
+                messageTwo.style.fontWeight='600'
                 messageTwo.textContent=`${data.temperature}℃ `
             messageOne.textContent=`Weather in ${data.location}`
             messageThree.textContent=`There is a ${data.rainfall}% chance of rain`
